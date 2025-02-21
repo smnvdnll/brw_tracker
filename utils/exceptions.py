bdbd7791f0b23878cd32ad9c2b1f0c3c7e26c96c
Daniil <dnk.semenov@gmail.com>
2025-02-21 14:56:05 +0300

    added more custom exceptions for handling BRW errors

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -1,3 +1,11 @@
+class RwException(Exception):
+    pass
 
-class TrainNotFoundException(Exception):
+class TrainNotFoundException(RwException):
+    pass
+
+class RwBackendException(RwException):
+    pass
+
+class InvalidResponseException(RwException):
     pass
