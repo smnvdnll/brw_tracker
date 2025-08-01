0cf4ea612fa4b9b5d6dff114302926e9a513a1df
Daniil <dnk.semenov@gmail.com>
2025-08-01 23:46:03 +0300

    fix: improved error handling in tracker manager

--- services/brw_manager.py
+++ services/brw_manager.py
@@ -3,14 +3,13 @@ import asyncio
 from datetime import datetime
 from typing import Any, Dict
 
-import backoff
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from common.lexicon import BRWMessages as lxc
 from database.models import BRWTracker
 from database.repository import BRWTrackerRepository
 from services.brw_api import BRWAPI
-from utils.exceptions import TrainNotFoundException
+from utils.exceptions import TrainNotFoundException, RwBackendException
 from utils.loggers import logger
 REQUEST_TIMEOUT = int(os.getenv("TIMEOUT_BETWEEN_REQUESTS"))
 
@@ -29,7 +28,6 @@ class BRWTrackerManager(BRWAPI):
         """
         Метод для получения полной информации об одном конкретном поезде
         """
-
         schedule = await self.get_schedule_for_date(
             self.brw_tracker.departure_station,
             self.brw_tracker.arrival_station,
@@ -38,40 +36,51 @@ class BRWTrackerManager(BRWAPI):
 
         # Если расписание не пустое
         if schedule is not None:
+
+            # Если какая-то внутрення ошибка БЧ
+            if schedule['e'].get('type') == "RwBackendException":
+                logger.error(f"Some RwBackendException. schedule['e']: {schedule['e']}")
+                raise RwBackendException()
+
             # Пытаемся найти поезд в расписании
-            try:
-                route = next(
-                    (
-                        route
-                        for route in schedule["routes"]
-                        if route["train_number"] == self.brw_tracker.train_number
-                    ),
-                    None,
-                )
+            # try:
+            logger.debug(f"Trying to find train")
+            route = next(
+                (
+                    route
+                    for route in schedule["routes"]
+                    if route["train_number"] == self.brw_tracker.train_number
+                ),
+                None,
+            )
 
-                # Поезд не был найден даже если было получено расписание, завершаем мониторинг
-                # TODO убедиться что поезд действительно не найден, а не какая то фигня методами ниже
-                if route is None:
-                    logger.debug(schedule)
-                    raise TrainNotFoundException()
-            except Exception as e:
-                logger.error(f"Failed to get train info: {e}")
-                return None
+            # Было получено нормальное расписание, но поезд найден все равно не был найден, завершаем мониторинг
+            if route is None:
+                raise TrainNotFoundException()
 
-        return route
+            return route
+        # а чо если schedule is None
 
-    def extract_places(self, route: Dict[str, str]) -> Dict[str, str]:
+    def extract_places(self, route: dict[str, str]) -> dict[str, str]:
         try:
+            logger.debug(f"Extracting places from route {route}")
+            # TODO заточено только под один тип места, надо будет сделать для всех
             new_places = {
                 place["classservice"]: place["places"]
                 for place in route["places"][0]["price_multi"]
             }
-            return new_places
-        except Exception as e:
-            logger.error(f"Failed to extract places: {e}. Route: {route}")
-            return {}
 
-    def compare_places(self, old_places: Dict[str, str], new_places: Dict[str, str]) -> str:
+            return new_places
+
+        # Места пустые (route['places'][0]['price_multi'] == [])
+        except IndexError:
+            logger.info(f"IndexError")
+        # TODO ак то более явно описать эту ошибку
+            return {}
+        except Exception as e:
+            logger.error(f"Unexcepted error while extracting places. Error: {e}")
+
+    def compare_places(self, old_places: dict[str, str], new_places: dict[str, str]) -> str:
         message = "Класс мест: старое количество -> новое количество\n"
         old_places = {} if old_places is None else old_places
         new_places = {} if new_places is None else new_places
@@ -97,28 +106,14 @@ class BRWTrackerManager(BRWAPI):
         old_places = None
 
         await self.send_to_telegram(lxc.monitoring_started)
-        while self.brw_tracker.is_turned_on:
-            await self.brw_db_repo.refresh(self.brw_tracker)
-            self.brw_tracker.is_turned_on = await self.brw_db_repo.is_tracker_turned_on(self.brw_tracker.id)
-
+        while self.brw_tracker.is_turned_on: # TODO может быть избавиться от параметра и проверять в целом на существование трекера в бд
             try:
                 route = await self.get_train_info()
-
-                # if attempt >= MAX_ATTEMPTS:
-                #     await self.send_to_telegram(lxc.max_attempts_exceeded)
-                #     # TODO возможно тут не надо сразу брейк, т.к. в 3 часа ночи проблема на стороне бч
-                #     break
-
-                # эта проверка возможно уже не нужна
-                # if route is None:
-                #     await self.send_to_telegram("route is none")
-                #     await asyncio.sleep(REQUEST_TIMEOUT)
-                #     continue
 
                 departure_time = datetime.strptime(
                     route["from_time_formatted"], "%Y-%m-%d %H:%M:%S"
                 )
-
+                
                 if datetime.now() >= departure_time:
                     await self.send_to_telegram(lxc.train_already_leaved)
                     break
@@ -132,13 +127,18 @@ class BRWTrackerManager(BRWAPI):
 
                 await asyncio.sleep(REQUEST_TIMEOUT)
 
-            except TrainNotFoundException:
+                await self.brw_db_repo.refresh(self.brw_tracker)
+                self.brw_tracker.is_turned_on = await self.brw_db_repo.is_tracker_turned_on(self.brw_tracker.id)
+
+            except TrainNotFoundException: # Поезд не был найден в расписании, завершаем мониторинг
                 await self.send_to_telegram(lxc.train_not_found)
                 logger.error(f"Train not found {self.brw_tracker}")
                 break
-            # TODO прочекать возможные исключения и обрабатывать их здесь
-            except Exception as e:
-                await self.send_to_telegram(f"Unexpected error: {e}")
+            except RwBackendException: # Ошибка на стороне БЧ
+                logger.error(f"RwBackendException {self.brw_tracker}")
+                await asyncio.sleep(REQUEST_TIMEOUT)
+            except Exception as e: # То что не попало в другие исключения
+                logger.critical(f"Unexpected error while monitoring: {e}. Tracker: {self.brw_tracker}")
                 await asyncio.sleep(REQUEST_TIMEOUT)
 
         await self.send_to_telegram(lxc.monitoring_ended)
