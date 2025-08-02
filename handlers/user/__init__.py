de303fbcbfa553d0413f335eb08a8ed372725027
Daniil <dnk.semenov@gmail.com>
2025-08-02 18:51:40 +0300

    initial commit

--- handlers/user/__init__.py
+++
@@ -1,5 +0,0 @@
-from aiogram import Router
-
-user_router = Router()
-
-from . import brw, common
--- main.py
+++ main.py
@@ -1,40 +1,6 @@
-import asyncio
-import logging
-import os
-
-from dotenv import find_dotenv, load_dotenv
-# load должен быть выше остальных импортов
-load_dotenv(find_dotenv(), override=True)
-
-from aiogram import Bot, Dispatcher
-from aiogram.types import BotCommand, BotCommandScopeAllPrivateChats, BotCommandScopeDefault
-
-from database.engine import create_all, sessionmaker
-from handlers.user import user_router
-from middlewares.database import DBSession
+def main():
+    print("Hello from brw-tracker!")
 
 
-
-async def main():
-    logging.basicConfig(level=logging.INFO)
-
-    bot = Bot(token=os.getenv("BOT_TOKEN"))
-    dp = Dispatcher()
-
-    dp.include_router(user_router)
-    dp.update.middleware(DBSession(session_pool=sessionmaker))
-
-    await bot.set_my_commands([
-            BotCommand(command="start", description="Главное меню"),
-        ],
-        scope=BotCommandScopeDefault()
-    )
-
-    await create_all()
-
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
-
-
-if __name__ == '__main__':
-    asyncio.run(main())
+if __name__ == "__main__":
+    main()
